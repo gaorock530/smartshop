df7dbc708ca1723566e8fbb08a8f2eb5daffa187
Ce Gao <magic@MagicBook-Pro.local>
2020-09-10 17:18:51 +0800

    update

--- src/components/buy.js
+++ src/components/buy.js
@@ -15,15 +15,11 @@ export default () => {
     const [longNav, setLongNav] = useState(true)
     let location = useLocation();
     let history = useHistory();
-  
-    // determine nav height
-    useEffect(() => {
-        const pwa = window.matchMedia('(display-mode: standalone)').matches
-        const length = history.length
-        const isWx = window.navigator.userAgent.toLowerCase().match(/micromessenger/i)
+    const length = history.length
 
-        setLongNav(pwa || (isWx && length < 2))
-    }, [location])
+    useEffect(() => {
+        setLongNav(isLongNav(length))
+    }, [location, length])
 
 
     return (
@@ -32,11 +28,27 @@ export default () => {
             <Link to='/shop/123' className="side"><FontAwesomeIcon icon={faStore}/></Link>
             <Link to='/shoppingchart' className="side"><FontAwesomeIcon icon={faShoppingCart}/></Link>
             <Link to='/' className="border">加入购物车</Link>
-    <Link to='/' className="border">{history.length}</Link>
+            <Link to='/' className="border">{length}</Link>
         </nav>
     )
 }
 
+
+function isLongNav(historyLength) {
+    const agent = window.navigator.userAgent;
+    const isiPhone = agent.match(/iphone/ig);
+    // uses device screen size determine iPhone model
+    const isWideEnough = window.screen.availHeight >= 812 && window.screen.availWidth >= 375;
+    const isDepthEnough = window.screen.colorDepth >= 32 || window.screen.pixelDepth >= 32;
+    const isSizeGood = isWideEnough && isDepthEnough;
+    // detect PWA
+    const isPWA = window.matchMedia('(display-mode: standalone)').matches
+    const isXPlus = isiPhone && isSizeGood;
+    // determine if inside WX
+    const isWx = agent.match(/micromessenger/ig)
+    
+    if (isXPlus && (isPWA || (isWx && historyLength < 2))) return true
+}
 // function useQuery() {
 //     return new URLSearchParams(useLocation().search);
 // }
--- src/router/home.js
+++ src/router/home.js
@@ -1,14 +1,21 @@
 import React from 'react'
-
+import Bowser from "bowser"
 
 
 export default () => {
-    // useEffect(() => {
-    //     console.log(window.navigator.userAgent)
-    // })
+    const browser = Bowser.parse(window.navigator.userAgent)
+    // Bowser.parse(window.navigator.userAgent)
+    console.log(browser)
 
 
     return (
-        <div>{window.navigator.userAgent}</div>
+        <div className="break-text">
+            <div>
+                <p>设备可用高度：{window.screen.availHeight}px</p>
+                <p>设备可用宽度：{window.screen.availWidth}px</p>
+                <p>设备色彩深度：{window.screen.colorDepth || window.screen.pixelDepth} bits</p>
+                <p>设备品牌型号：{JSON.stringify(browser)}</p>
+            </div>
+        </div>
     )
 }
